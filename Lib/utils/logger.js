c9a8ed2d5aa363d087204e76c26d141fb1a1c8f4
SACreations <supunadithya@hotmail.com>
2025-03-31 13:16:49 +0530

    feat: update import statement for 'downloadMediaMessage' to remove package prefix

--- Lib/utils/logger.js
+++ Lib/utils/logger.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import { downloadMediaMessage } from '@whiskeysockets/baileys';
+import { downloadMediaMessage } from 'baileys';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
